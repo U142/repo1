74f02521c9bef6527015e958a94aeccabb1d4801
Morten Helvik <mh@ums.no>
2010-10-13 13:07:02 +0000

    Oppdatering av 1.00-b30

--- src/no/ums/pas/core/ws/vars.java
+++ src/no/ums/pas/core/ws/vars.java
@@ -14,24 +14,22 @@ public class vars
 	
 	public static void init(String sz_base)
 	{
+		/**
+		 * ULOVLIG Å ENDRE TIL LOCALHOST
+		 */
 		WSDL_CONFERENCE 		= sz_base + "Conference.asmx?WSDL";
 		
 		WSDL_EXTERNALEXEC 		= sz_base + "ExternalExec.asmx?WSDL";
-		//WSDL_EXTERNALEXEC		= "http://localhost/WS/ExternalExec.asmx?WSDL";
 		
 		WSDL_INFOSENTRAL		= sz_base + "Infosentral.asmx?WSDL";
 		
 		WSDL_PARMADMIN 			= sz_base + "ParmAdmin.asmx?WSDL";
-		//WSDL_PARMADMIN 			= "http://localhost/WS/ParmAdmin.asmx?WSDL";
 
 		WSDL_PAS 				= sz_base + "PAS.asmx?WSDL";
-		//WSDL_PAS 				= "http://localhost/WS/PAS.asmx?WSDL";
 		
-		WSDL_PASSTATUS			= "http://localhost/WS/PasStatus.asmx?WSDL";
-		//WSDL_PASSTATUS			= sz_base + "PasStatus.asmx?WSDL";
+		WSDL_PASSTATUS			= sz_base + "PasStatus.asmx?WSDL";
 		
 		WSDL_TAS				= sz_base + "TAS.asmx?WSDL";
-		//WSDL_TAS				= "http://localhost/WS/TAS.asmx?WSDL";
 		WSDL_VB					= sz_base + "VB.asmx?WSDL";
 		WSDL_VOICE				= sz_base + "Voice.asmx?WSDL";
 	}
