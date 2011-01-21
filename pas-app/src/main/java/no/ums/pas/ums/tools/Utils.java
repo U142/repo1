92ee5416bf129c403d19f0945396f0b971ed4fd9
Ståle Undheim <staaleu@ums.no>
2011-01-21 09:57:30 +0000

    Formatering

--- pas-app/src/main/java/no/ums/pas/ums/tools/Utils.java
+++ pas-app/src/main/java/no/ums/pas/ums/tools/Utils.java
@@ -205,8 +205,7 @@ public final class Utils {
         }
         final StringBuilder hex = new StringBuilder(2 * raw.length);
         for (final byte b : raw) {
-            hex.append(HEXES.charAt((b & 0xF0) >> 4))
-                    .append(HEXES.charAt((b & 0x0F)));
+            hex.append(HEXES.charAt((b & 0xF0) >> 4)).append(HEXES.charAt((b & 0x0F)));
         }
         return hex.toString();
     }
