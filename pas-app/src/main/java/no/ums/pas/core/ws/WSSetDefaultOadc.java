5babcefc236d9068637201e2bc041a736b458af1
Morten Helvik <mh@ums.no>
2011-02-18 10:56:08 +0000

    glemte å legge til disse filene

---
+++ pas-app/src/main/java/no/ums/pas/core/ws/WSSetDefaultOadc.java
@@ -0,0 +1,59 @@
+package no.ums.pas.core.ws;
+
+import java.awt.Component;
+import java.net.URL;
+
+import javax.xml.namespace.QName;
+
+import no.ums.pas.PAS;
+import no.ums.ws.pas.MDSOADCDEFAULT;
+import no.ums.ws.pas.Pasws;
+
+public class WSSetDefaultOadc extends WSThread
+{
+	private String oadc;
+	private MDSOADCDEFAULT ret = null;
+	public MDSOADCDEFAULT getReturnValue() { return ret; }
+	public WSSetDefaultOadc(String oadc)
+	{
+		super(null);
+		this.oadc = oadc;
+	}
+
+	@Override
+	protected String getErrorMessage() {
+		return null;
+	}
+
+	@Override
+	public void onDownloadFinished() {
+		
+	}
+
+	@Override
+	public void call() throws Exception {
+		no.ums.ws.pas.ObjectFactory of = new no.ums.ws.pas.ObjectFactory();
+		no.ums.ws.pas.ULOGONINFO logon = of.createULOGONINFO();
+		logon.setLComppk(PAS.get_pas().get_userinfo().get_comppk());
+		logon.setLDeptpk(PAS.get_pas().get_userinfo().get_current_department().get_deptpk());
+		logon.setLUserpk(new Long(PAS.get_pas().get_userinfo().get_userpk()));
+		logon.setSzPassword(PAS.get_pas().get_userinfo().get_passwd());
+		logon.setSessionid(PAS.get_pas().get_userinfo().get_sessionid());
+		try
+		{
+			URL wsdl = new URL(vars.WSDL_PAS);
+			QName service = new QName("http://ums.no/ws/pas/", "pasws");
+			ret = new Pasws(wsdl, service).getPaswsSoap12().setDefaultOadc(logon, oadc);
+        	PAS.get_pas().get_userinfo().set_default_oadc(getReturnValue().getSzValue());
+        	PAS.get_pas().get_userinfo().set_default_oadc_type(getReturnValue().getLType());
+		}
+		catch(Exception e)
+		{
+			throw e;
+		}
+		finally
+		{
+		}
+
+	}
+}
---
+++ pas-app/src/main/java/no/ums/pas/send/sendpanels/SendPanelActions.java
@@ -0,0 +1,17 @@
+package no.ums.pas.send.sendpanels;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.Action;
+
+import no.ums.log.Log;
+import no.ums.log.UmsLog;
+import no.ums.pas.core.ws.WSSetDefaultOadc;
+import no.ums.pas.swing.UmsAction;
+
+public interface SendPanelActions
+{
+    Log log = UmsLog.getLogger(SendPanelActions.class);
+
+
+}
