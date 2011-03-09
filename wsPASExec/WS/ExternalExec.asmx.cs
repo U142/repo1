d4d69c94bea5cc761c33783ac7552554c0ed1aec
Svein Anfinnsen <sa@ums.no>
2011-03-09 09:15:03 +0000

    Fikset GetAdrCount

--- wsPASExec/WS/ExternalExec.asmx.cs
+++ wsPASExec/WS/ExternalExec.asmx.cs
@@ -252,8 +252,7 @@ namespace com.ums.ws.parm
                    (mapsending.n_addresstypes & (long)ADRTYPES.SMS_PRIVATE) > 0 ||
                    (mapsending.n_addresstypes & (long)ADRTYPES.SMS_PRIVATE_ALT_FIXED) > 0)
                 {
-                    //return new UAdrDb(logon.sz_stdcc, 120, logon.l_deptpk).GetAddressCount(ref logon, ref mapsending);
-                    return GetAddressCount(ref mapsending);
+                    return new UAdrDb(logon.sz_stdcc, 120, logon.l_deptpk).GetAddressCount(ref logon, ref mapsending);
                 }
                 else
                 {
