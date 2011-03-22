057510dbf0b287669ad6419e16bb9e8e2198d3ee
Morten Helvik <mh@ums.no>
2011-03-22 13:14:49 +0000

    Fikset på sanitytest så den peker mot korrekt webservice (på ums2.no)

--- pas-app/src/test/java/no/ums/pas/SanityTest.java
+++ pas-app/src/test/java/no/ums/pas/SanityTest.java
@@ -1,5 +1,11 @@
 package no.ums.pas;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.xml.namespace.QName;
+
+import no.ums.pas.core.ws.vars;
 import no.ums.ws.pas.Pasws;
 import org.junit.Test;
 
@@ -9,7 +15,16 @@ import org.junit.Test;
 public class SanityTest {
 
     @Test
-    public void verifyOneTimeKeyWorks() {
-        new Pasws().getPaswsSoap12().getOneTimeKey();
+    public void verifyOneTimeKeyWorks() throws MalformedURLException {
+    	try
+    	{
+	    	URL wsdl = new URL("https://secure.ums2.no/pas/ws_pasutv/ws/PAS.asmx");
+			QName service = new QName("http://ums.no/ws/pas/", "pasws");
+	        new Pasws(wsdl, service).getPaswsSoap12().getOneTimeKey();
+    	}
+    	finally
+    	{
+    		
+    	}
     }
 }
