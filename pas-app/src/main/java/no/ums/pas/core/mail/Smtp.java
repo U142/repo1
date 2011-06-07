71a821af89f95dc459945aa820cae9ae8a0e2afc
Ståle Undheim <staaleu@ums.no>
2011-06-07 10:57:55 +0000

    Fjernet java mail imports

--- pas-app/src/main/java/no/ums/pas/core/mail/Smtp.java
+++ pas-app/src/main/java/no/ums/pas/core/mail/Smtp.java
@@ -1,19 +1,10 @@
 package no.ums.pas.core.mail;
 
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.Socket;
 import java.util.List;
-import java.util.Properties;
 
 public class Smtp implements Runnable
 {
