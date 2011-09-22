b09cfe0c68e5165d663fffee267db7c9c4c817ac
Svein Anfinnsen <sa@ums.no>
2011-09-22 11:08:44 +0200

    Test for å se hva som skjer med en annen encoding

--- pas-java-client/pas-app/src/main/java/no/ums/pas/importer/gis/PreviewOptions.java
+++ pas-java-client/pas-app/src/main/java/no/ums/pas/importer/gis/PreviewOptions.java
@@ -76,7 +76,7 @@ class PreviewOptions extends DefaultPanel {
 		m_check_firstline_columnnames.setActionCommand("act_first_row_has_columnnames");
         m_btn_fetch = new JButton((m_b_is_alert ? Localization.l("common_preview") : Localization.l("common_wizard_next")));
 		m_lbl_encoding = new JLabel(Localization.l("importpreview_encoding"));
-		m_cbx_encoding = new JComboBox(new String[] { "ISO-8859-15", "UTF-8" });
+		m_cbx_encoding = new JComboBox(new String[] { "ISO-8859-15", "UTF-8", "ISO-8859-2" });
 		m_cbx_encoding.setActionCommand("act_change_encoding");
 		m_cbx_encoding.addActionListener(this);
 		m_btn_fetch.addActionListener(this);
