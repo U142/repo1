e040f464306a39a5fde6716e8da9ecfc28e56bf8
Morten Helvig <mh@ums.no>
2011-12-07 15:01:48 +0100

    APP-193
    Sett språk i VB4 for visning av meldingsprofil.
    Funker for norsk og engelsk, er også klargjort for spansk, dansk og svensk.
    
    Krever release av vb4utv/PAS_logon_auto.asp til maya/misato/ny orange

--- pas-java-client/pas-app/src/main/java/no/ums/pas/send/sendpanels/ShowProfileAction.java
+++ pas-java-client/pas-app/src/main/java/no/ums/pas/send/sendpanels/ShowProfileAction.java
@@ -6,6 +6,7 @@ import no.ums.log.Log;
 import no.ums.log.UmsLog;
 import no.ums.pas.PAS;
 import no.ums.pas.core.logon.UserInfo;
+import no.ums.pas.localization.Localization;
 import no.ums.pas.swing.UmsAction;
 
 import java.awt.Desktop;
@@ -34,13 +35,24 @@ public class ShowProfileAction extends UmsAction {
     public void actionPerformed(ActionEvent e) {
         UserInfo userinfo = PAS.get_pas().get_userinfo();
         try {
-            String url = String.format("%s/PAS_msg_profile_dlg.asp?f_setreadonly=True&lProfilePk=%s&l_deptpk=%s&usr=%s&cmp=%s&pas=%s",
+        	String szLanguage = "eng";
+        	String szLocale = Localization.INSTANCE.getLocale().getCountry();
+        	if(szLocale.equals("NO"))
+        		szLanguage = "nor";
+        	else if(szLocale.equals("ES"))
+        		szLanguage = "spa";
+        	else if(szLocale.equals("DK"))
+        		szLanguage = "dan";
+        	else if(szLocale.equals("SE"))
+        		szLanguage = "sve";
+            String url = String.format("%s/PAS_msg_profile_dlg.asp?f_setreadonly=True&lProfilePk=%s&l_deptpk=%s&usr=%s&cmp=%s&pas=%s&xlang=%s",
                     PAS.get_pas().getVB4Url(),
                     URLEncoder.encode(String.valueOf(profilePkSupplier.get()), "UTF-8"),
                     URLEncoder.encode(String.valueOf(userinfo.get_default_dept().get_deptpk()), "UTF-8"),
                     URLEncoder.encode(userinfo.get_userid(), "UTF-8"),
                     URLEncoder.encode(userinfo.get_compid(), "UTF-8"),
-                    URLEncoder.encode(userinfo.get_passwd(), "UTF-8")
+                    URLEncoder.encode(userinfo.get_passwd(), "UTF-8"),
+                    URLEncoder.encode(szLanguage, "UTF-8")
             );
             try {
                 Desktop.getDesktop().browse(URI.create(url));
