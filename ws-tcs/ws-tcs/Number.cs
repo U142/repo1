446bda2cab93accca5f9492c756fe453d44bb7f5
Svein Anfinnsen <sa@ums.no>
2012-03-20 15:54:27 +0100

    PLF-437 Lagt til tcs web service

---
+++ ws-tcs/ws-tcs/Number.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Web.Services.Protocols;
+using System.Xml.Serialization;
+
+namespace ums.ws.tcs
+{
+    public class Number
+    {
+        static readonly Regex All_Numeric_Regex = new Regex("[^0-9]");
+        static readonly Regex GSM_Regular_Regex = new Regex("[^a-zA-Z0-9 .∆_ΦΓΛΩΠΨΣΘΞ@£$¥èéùìòÇØøÅåÆæßÉÄÖÑÜ§¿äöñüà+,/:;<=>?¡*!#¤%&'()\"-]");
+
+        private static bool isMSISDN(string Number)
+        {
+            bool ret = false;
+
+            if (Number.Length==10 && Number.StartsWith("47") && AllNumeric(Number))
+                ret = true;
+
+            return ret;
+        }
+        private static bool AllNumeric(string inputString)
+        {
+            if (All_Numeric_Regex.IsMatch(inputString))
+            {
+                return false;
+            }
+            return true;
+        }
+        public static int GetOtoa(string Oadc)
+        {
+            int ret = 1; // default alphanumeric
+
+            if (GSM_Regular_Regex.IsMatch(Oadc))
+            {
+                // Non-GSM characters found in sender address, return error
+                ret = -1;
+            }
+            else
+            {
+                if (Oadc.Length >= 2)
+                    if (Oadc.StartsWith("+") && AllNumeric(Oadc.Substring(1)))
+                        Oadc = Oadc.Replace("+", "00");
+
+                if (AllNumeric(Oadc))	// originating address is numeric, must format correctly for IPX
+                {
+                    if (Oadc.Length >= 7 && Oadc.Length <= 16) // MSISDN (max 16)
+                    {
+                        ret = 2;
+                    }
+                    else if (Oadc.Length <= 11) // Shortnumber (send as alphanumeric, max 11 characters)
+                    {
+                        //ret = 0; // shortnumber, try to avoid usage
+                        ret = 1;
+                    }
+                    else
+                    {
+                        ret = -1;
+                    }
+                }
+                else if (Oadc.Length <= 11) // Alphanumeric (max 11)
+                {
+                    ret = 1;
+                }
+                else
+                {
+                    ret = -1;
+                }
+            }
+
+            return ret;
+        }
+        public static ERROR CheckMessage(Message message, UserValues uv)
+        {
+            ERROR ret = ERROR.success;
+
+            if (message==null || message.text.Trim() == "") // empty message not allowed
+                return ERROR.sending_nomessage;
+
+            if (message.text.Length > umssettings._settings.GetValue("MaxMessageLength", 160)) // 160 default, 500 max (AlertiX)
+                return ERROR.sending_messagetoolong;
+
+            if (GSM_Regular_Regex.IsMatch(message.text)) // contains non-gsm characters
+                return ERROR.sending_messageinvalidgsm;
+
+            if (message.mode == ExecuteMode.LIVE && !uv.f_live)
+                return ERROR.sending_nosend;
+
+            if (message.mode == ExecuteMode.SIMULATE && !uv.f_simulate)
+                return ERROR.sending_nosimulate;
+
+            return ret;
+        }
+
+        public static HashSet<string> parse(List<string> number, List<string> invalidNumbers)
+        {
+            HashSet<string> ret = new HashSet<string>();
+
+            try
+            {
+                foreach (string s in number)
+                {
+                    string addNumber = s;
+
+                    // allow 0047xxxxxxxx and +47xxxxxxxx in addition to 47xxxxxxxx
+                    if ((s.StartsWith("0047") && s.Length == 12) || (s.StartsWith("+47") && s.Length == 11))
+                    {
+                        addNumber = s.Substring(s.Length - 8);
+                    }
+
+                    if (isMSISDN(addNumber))
+                    {
+                        ret.Add(addNumber);
+                   }
+                    else
+                    {
+                        // use original number in invalidNumbers list
+                        invalidNumbers.Add(s);
+                    }
+                }
+            }
+            catch (SoapException)
+            {
+                throw;
+            }
+            catch (Exception e)
+            {
+                throw new SoapException(e.Message, SoapException.ServerFaultCode);
+            }
+                        
+            return ret;
+        }
+
+        public static List<string> StreamToList(byte[] file)
+        {
+            List<string> number = new List<string>();
+
+            try
+            {
+                Stream s = new MemoryStream(file);
+                StreamReader f = new StreamReader(s);
+
+                while (!f.EndOfStream)
+                {
+                    number.Add(f.ReadLine());
+                }
+
+                f.Close();
+                s.Close();
+            }
+            catch (SoapException)
+            {
+                throw;
+            }
+            catch (Exception e)
+            {
+                throw new SoapException(e.Message, SoapException.ServerFaultCode);
+            }
+
+            return number;
+        }
+
+        public static string SerializeList(List<string> numbers)
+        {
+            XmlSerializer s = new XmlSerializer(typeof(List<string>));
+            TextWriter w = new StringWriter();
+            s.Serialize(w, numbers);
+            return w.ToString();
+        }
+        public static List<string> DeserializeList(string xml)
+        {
+            XmlSerializer s = new XmlSerializer(typeof(List<string>));
+            TextReader r = new StringReader(xml);
+            return (List<string>)s.Deserialize(r);
+        }
+    }
+}
---
+++ ws-tcs/ws-tcs/TCSXmlWriter.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace ums_tcs_lib
+{
+    enum TCS_ERROR
+    {
+        SUCCESS,
+        ERROR,
+        EXISTS,
+        UNDEFINED,
+        NOTIPLEMENTED,
+        EMPTY
+    }
+
+    class TCSXmlWriter
+    {
+        private static string getSendFQFN(string path, int refno)
+        {
+            if (Directory.Exists(path) == false)
+                throw new Exception("Path does not exist.");
+            if (path.EndsWith("\\") || path.EndsWith("/"))
+                return String.Format("{0}TAS_SEND_{1}.tmp", path, refno);
+            return String.Format("{0}\\TAS_SEND_{1}.tmp", path, refno);
+        }
+
+        private static string getConfirmFQFN(string path, string jobid, int refno)
+        {
+            if (Directory.Exists(path) == false)
+                throw new Exception("Path does not exists.");
+            if (path.EndsWith("\\") || path.EndsWith("/"))
+                return String.Format("{0}TAS_CONFIRM_{1}.{2}.tmp", path, jobid, refno);
+            return String.Format("{0}\\TAS_CONFIRM_{1}.{2}.tmp", path, jobid, refno);
+        }
+
+        private static string getFQFN(string path)
+        {
+            if (Directory.Exists(path) == false)
+                throw new Exception("Path does not exist.");
+            if (path.EndsWith("\\") || path.EndsWith("/"))
+                return String.Format("{0}TAS_Admin_{1}.tmp", path, System.Guid.NewGuid().ToString());
+            return String.Format("{0}\\TAS_Admin_{1}.tmp", path, System.Guid.NewGuid().ToString());
+        }
+
+        public static void TCSListCreate(
+            string path,
+            string listname,
+            decimal listpk,
+            int comppk,
+            int deptpk)
+        {
+            String FQFN = getFQFN(path);
+            XmlTextWriter xml = new XmlTextWriter(FQFN, Encoding.UTF8);
+            xml.WriteStartDocument();
+            {
+                xml.WriteWhitespace("\r\n");
+                xml.WriteStartElement("TAS");
+                {
+                    xml.WriteStartAttribute("operation");
+                    xml.WriteString("CreateList");
+                    xml.WriteEndAttribute();
+                    xml.WriteStartAttribute("l_listpk");
+                    xml.WriteString(listpk.ToString());
+                    xml.WriteEndAttribute();
+                    xml.WriteStartAttribute("l_comppk");
+                    xml.WriteString(comppk.ToString());
+                    xml.WriteEndAttribute();
+                    xml.WriteStartAttribute("l_deptpk");
+                    xml.WriteString(deptpk.ToString());
+                    xml.WriteEndAttribute();
+                    xml.WriteWhitespace("\r\n  ");
+                    xml.WriteStartElement("whitelist");
+                    {
+                        xml.WriteStartAttribute("name");
+                        xml.WriteString(listname);
+                        xml.WriteEndAttribute();
+                    }//</whitelist>
+                    xml.WriteEndElement();
+                    xml.WriteWhitespace("\r\n");
+                }//</TAS>
+                xml.WriteEndElement();
+            }
+            xml.WriteEndDocument();
+            xml.Close();
+            String FFQFN = FQFN.Replace(".tmp", ".xml");
+            File.Move(FQFN, FFQFN);
+        }
+
+        public static void TCSListDelete(
+            string path,
+            string listname,
+            decimal listpk,
+            int comppk,
+            int deptpk)
+        {
+            String FQFN = getFQFN(path);
+            XmlTextWriter xml = new XmlTextWriter(FQFN, Encoding.UTF8);
+            xml.WriteStartDocument();
+            {
+                xml.WriteWhitespace("\r\n");
+                xml.WriteStartElement("TAS");
+                {
+                    xml.WriteStartAttribute("operation");
+                    xml.WriteString("DeleteList");
+                    xml.WriteEndAttribute();
+                    xml.WriteStartAttribute("l_listpk");
+                    xml.WriteString(listpk.ToString());
+                    xml.WriteEndAttribute();
+                    xml.WriteStartAttribute("l_comppk");
+                    xml.WriteString(comppk.ToString());
+                    xml.WriteEndAttribute();
+                    xml.WriteStartAttribute("l_deptpk");
+                    xml.WriteString(deptpk.ToString());
+                    xml.WriteEndAttribute();
+                    xml.WriteWhitespace("\r\n  ");
+                    xml.WriteStartElement("whitelist");
+                    {
+                        xml.WriteStartAttribute("name");
+                        xml.WriteString(listname);
+                        xml.WriteEndAttribute();
+                    }//</whitelist>
+                    xml.WriteEndElement();
+                    xml.WriteWhitespace("\r\n");
+                }//</TAS>
+                xml.WriteEndElement();
+            }
+            xml.WriteEndDocument();
+            xml.Close();
+            String FFQFN = FQFN.Replace(".tmp", ".xml");
+            File.Move(FQFN, FFQFN);
+        }
+
+        public static void TCSListUpdate(
+            string path,
+            HashSet<string> add,
+            HashSet<string> rem,
+            string listname,
+            decimal listpk,
+            int comppk,
+            int deptpk)
+        {
+            if (add == null && rem == null)
+                throw new Exception("Both lists are null");
+            if (add.Count == 0 && rem.Count == 0)
+                throw new Exception("Both lists are empty");
+
+            String FQFN = getFQFN(path);
+            XmlTextWriter xml = new XmlTextWriter(FQFN, Encoding.UTF8);
+
+            xml.WriteStartDocument();
+            {
+                xml.WriteWhitespace("\r\n");
+                xml.WriteStartElement("TAS");
+                {
+                    xml.WriteStartAttribute("operation");
+                    xml.WriteString("UpdateWhitelist");
+                    xml.WriteEndAttribute();
+                    xml.WriteStartAttribute("l_listpk");
+                    xml.WriteString(listpk.ToString());
+                    xml.WriteEndAttribute();
+                    xml.WriteStartAttribute("l_comppk");
+                    xml.WriteString(comppk.ToString());
+                    xml.WriteEndAttribute();
+                    xml.WriteStartAttribute("l_deptpk");
+                    xml.WriteString(deptpk.ToString());
+                    xml.WriteEndAttribute();
+                    xml.WriteWhitespace("\r\n  ");
+                    xml.WriteStartElement("whitelist");
+                    {
+                        xml.WriteStartAttribute("name");
+                        xml.WriteString(listname);
+                        xml.WriteEndAttribute();
+                        foreach (string s in add)
+                        {
+                            xml.WriteWhitespace("\r\n    ");
+                            xml.WriteStartElement("add");
+                            xml.WriteString(s);
+                            xml.WriteEndElement();
+                        }
+                        foreach (string s in rem)
+                        {
+                            xml.WriteWhitespace("\r\n    ");
+                            xml.WriteStartElement("rem");
+                            xml.WriteString(s);
+                            xml.WriteEndElement();
+                        }
+                        xml.WriteWhitespace("\r\n  ");
+                    }//</whitelist>
+                    xml.WriteEndElement();
+                    xml.WriteWhitespace("\r\n");
+                }//</TAS>
+                xml.WriteEndElement();
+            }
+            xml.WriteEndDocument();
+            xml.Close();
+            String FFQFN = FQFN.Replace(".tmp", ".xml");
+            File.Move(FQFN, FFQFN);
+        }
+
+        public static void TCSSendLBA(
+            string path,
+            int refno,
+            int comppk,
+            int deptpk,
+            int simulate,
+            int validity,
+            int requesttype,
+            int otoa,
+            string text,
+            string oadc,
+            HashSet<int> cc,
+            HashSet<string> lists)
+        {
+            String FQFN = getSendFQFN(path, refno);
+            XmlTextWriter xml = new XmlTextWriter(FQFN, Encoding.UTF8);
+            xml.WriteStartDocument();
+            {
+                xml.WriteWhitespace("\r\n");
+                xml.WriteStartElement("LBA");
+                {
+                    xml.WriteStartAttribute("operation");
+                    xml.WriteString("SendInternational");
+                    xml.WriteEndAttribute();
+                    xml.WriteStartAttribute("l_refno");
+                    xml.WriteString(refno.ToString());
+                    xml.WriteEndAttribute();
+                    xml.WriteStartAttribute("l_comppk");
+                    xml.WriteString(comppk.ToString());
+                    xml.WriteEndAttribute();
+                    xml.WriteStartAttribute("l_deptpk");
+                    xml.WriteString(deptpk.ToString());
+                    xml.WriteEndAttribute();
+                    xml.WriteStartAttribute("f_simulation");
+                    xml.WriteString(simulate.ToString());
+                    xml.WriteEndAttribute();
+                    xml.WriteStartAttribute("l_validity");
+                    xml.WriteString(validity.ToString());
+                    xml.WriteEndAttribute();
+                    xml.WriteStartAttribute("l_requesttype");
+                    xml.WriteString(requesttype.ToString());
+                    xml.WriteEndAttribute();
+
+                    xml.WriteWhitespace("\r\n  ");
+                    xml.WriteStartElement("textmessages");
+                    {
+                        xml.WriteWhitespace("\r\n    ");
+                        xml.WriteStartElement("message");
+                        {
+                            xml.WriteStartAttribute("sz_text");
+                            xml.WriteString(text);
+                            xml.WriteEndAttribute();
+                            xml.WriteStartAttribute("sz_cb_oadc");
+                            xml.WriteString(oadc);
+                            xml.WriteEndAttribute();
+                            xml.WriteStartAttribute("l_oatoa");
+                            xml.WriteString(otoa.ToString());
+                            xml.WriteEndAttribute();
+                            foreach (int i in cc)
+                            {
+                                xml.WriteWhitespace("\r\n      ");
+                                xml.WriteStartElement("ccode");
+                                xml.WriteString(i.ToString());
+                                xml.WriteEndElement();
+                            }
+                            xml.WriteWhitespace("\r\n    ");
+                        }//<message>
+                        xml.WriteEndElement();
+                        xml.WriteWhitespace("\r\n  ");
+                    }//<textmessags>
+                    xml.WriteEndElement();
+                    xml.WriteWhitespace("\r\n  ");
+                    xml.WriteStartElement("whitelists");
+                    {
+                        foreach (string s in lists)
+                        {
+                            xml.WriteWhitespace("\r\n    ");
+                            xml.WriteStartElement("whitelist");
+                            {
+                                xml.WriteStartAttribute("name");
+                                xml.WriteString(s);
+                                xml.WriteEndAttribute();
+                            }//</whitelist>
+                            xml.WriteEndElement();
+                        }
+                        xml.WriteWhitespace("\r\n  ");
+                    }//</whitelists>
+                    xml.WriteEndElement();
+                    xml.WriteWhitespace("\r\n");
+                }//</LBA>
+                xml.WriteEndElement();
+            }
+            xml.WriteEndDocument();
+            xml.Close();
+            String FFQFN = FQFN.Replace(".tmp", ".xml");
+            File.Move(FQFN, FFQFN);
+        }
+
+        public static void TCSConfirmLBA(
+            string path,
+            int refno,
+            int comppk,
+            int deptpk,
+            int simulate,
+            int oper,
+            string jobid,
+            bool cancel)
+        {
+            String FQFN = getConfirmFQFN(path, jobid, refno);
+            XmlTextWriter xml = new XmlTextWriter(FQFN, Encoding.UTF8);
+            xml.WriteStartDocument();
+            xml.WriteWhitespace("\r\n");
+            xml.WriteStartElement("LBA");
+            xml.WriteStartAttribute("operation");
+            if (cancel)
+                xml.WriteString("CancelInternational");
+            else
+                xml.WriteString("ConfirmInternational");
+            xml.WriteEndAttribute();
+            xml.WriteStartAttribute("l_refno");
+            xml.WriteString(refno.ToString());
+            xml.WriteEndAttribute();
+            xml.WriteStartAttribute("sz_jobid");
+            xml.WriteString(jobid);
+            xml.WriteEndAttribute();
+            xml.WriteStartAttribute("f_simulation");
+            xml.WriteString(simulate.ToString());
+            xml.WriteEndAttribute();
+            xml.WriteStartAttribute("l_operator");
+            xml.WriteString(oper.ToString());
+            xml.WriteEndAttribute();
+            xml.WriteStartAttribute("l_deptpk");
+            xml.WriteString(deptpk.ToString());
+            xml.WriteEndAttribute();
+            xml.WriteStartAttribute("l_comppk");
+            xml.WriteString(comppk.ToString());
+            xml.WriteEndAttribute();
+            xml.WriteEndElement();
+            xml.WriteEndDocument();
+            xml.Close();
+            String FFQFN = FQFN.Replace(".tmp", ".xml");
+            File.Move(FQFN, FFQFN);
+        }
+    }
+}
