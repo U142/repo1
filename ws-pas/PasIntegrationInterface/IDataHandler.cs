c6e67e91d2974a1a1908bb1ed9e71ad389ce9ee7
Morten H. Helvig <mh@ums.no>
2013-02-25 15:04:57 +0100

    PWI-22 Changed dependencies
    Conflicts:
    
    	ws-pas/wsPASExec.sln
    	ws-pas/wsPASExec/wsPASExec.csproj

---
+++ ws-pas/PasIntegrationInterface/IDataHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace com.ums.pas.integration
+{
+    public interface IDataHandler
+    {
+        void HandleAlert(AlertMqPayload payload);
+
+    }
+}
