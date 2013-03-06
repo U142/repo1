b9b0659df405598d80f017538c9db4436c4d32ed
Morten H. Helvig <mh@ums.no>
2013-03-06 09:58:05 +0100

    PWI-66 Functionality for starting service in user interactive mode
    Added functionality for console if user manually starts the service.
    Also publishing towards apps1 beta.

--- ws-pas/PasIntegrationService/IntegrationService.cs
+++ ws-pas/PasIntegrationService/IntegrationService.cs
@@ -3,11 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.ServiceProcess;
 using System.Text;
+using System.Runtime.InteropServices;
+using System.Threading;
+using com.ums.UmsCommon;
+using System.Threading.Tasks;
 
 namespace com.ums.pas.integration
 {
     static class IntegrationService
     {
+        [DllImport("kernel32.dll",
+            EntryPoint = "GetStdHandle",
+            SetLastError = true,
+            CharSet = CharSet.Auto,
+            CallingConvention = CallingConvention.StdCall)]
+        private static extern IntPtr GetStdHandle(int nStdHandle);
+        [DllImport("kernel32.dll",
+            EntryPoint = "AllocConsole",
+            SetLastError = true,
+            CharSet = CharSet.Auto,
+            CallingConvention = CallingConvention.StdCall)]
+        private static extern int AllocConsole();
+        private const int STD_OUTPUT_HANDLE = -11;
+        private const int MY_CODE_PAGE = 437; 
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -24,8 +43,27 @@ namespace com.ums.pas.integration
             }
             else
             {
-                new Integration().StartActiveMq();
+                AllocConsole();
+                Console.WriteLine("Starting in UserInteractive Mode");
+                Integration integration = new Integration();
+                Thread thread = new Thread(() => integration.StartActiveMq()); //new ThreadStart(integration.StartActiveMq));
+                thread.Start();
+                while (true)
+                {
+                    String lineString = Console.ReadLine();
+                    if (lineString.ToUpper().Equals("Q"))
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine("Stopping service, Waiting for thread to finish...");
+                integration.Stop();
+                thread.Join();
+
+
             }
         }
+
+      
     }
 }
