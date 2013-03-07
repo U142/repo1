cee52a60c39197f3887d1a712e2c3ab98c03e247
Morten H. Helvig <mh@ums.no>
2013-03-07 12:16:57 +0100

    PWI-66 Added support for TTS
    problems with writing rights do unc
    
    Conflicts:
    
    	ws-pas/UmsAudio/AudioClasses.cs
    	ws-pas/UmsDbLib/UmsDbLib.cs

---
+++ ws-pas/PasIntegrationInterface/ITtsFacade.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace com.ums.pas.integration
+{
+    /// <summary>
+    /// Facade interface for handling conversion of TTS.
+    /// For simplicity, conversions here will return byte arrays.
+    /// In that manner the application will be responsible to write as files to the system where needed.
+    /// </summary>
+    public interface ITtsFacade
+    {
+        /// <summary>
+        /// Convert TTS and return raw audiofile
+        /// </summary>
+        /// <param name="Text"></param>
+        /// <param name="Speaker"></param>
+        /// <param name="DestinationFile"></param>
+        /// <returns>File content</returns>
+        byte [] ConvertTtsRaw(String Text, String Speaker, String DestinationFile);
+
+        /// <summary>
+        /// Convert TTS and return wav audiofile
+        /// </summary>
+        /// <param name="Text"></param>
+        /// <param name="Speaker"></param>
+        /// <param name="DestinationFile"></param>
+        /// <returns>File content</returns>
+        byte [] ConvertTtsWav(String Text, String Speaker, String DestinationFile);
+    }
+}
---
+++ ws-pas/PasIntegrationService/TtsFacadeImpl.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using com.ums.UmsCommon.Audio;
+
+namespace com.ums.pas.integration
+{
+    public class TtsFacadeImpl : ITtsFacade
+    {
+        #region ITtsFacade Members
+
+        public byte[] ConvertTtsRaw(string Text, string Speaker, string DestinationFile)
+        {
+
+            return null;
+        }
+
+        public byte[] ConvertTtsWav(string Text, string Speaker, string DestinationFile)
+        {
+            UAudio audio = new UAudio();
+            UCONVERT_TTS_REQUEST ttsReq = new UCONVERT_TTS_REQUEST();
+            UCONVERT_TTS_RESPONSE ttsResponse = audio.ConvertTTS(ttsReq);
+            if(ttsResponse.n_responsecode==0)
+                return ttsResponse.wav;
+            throw new Exception(ttsResponse.sz_responsetext);
+        }
+
+        #endregion
+    }
+}
