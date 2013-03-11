d841a8314c6687de25fbbdbb66fbae82eeeb8ae0
Morten H. Helvig <mh@ums.no>
2013-03-11 15:04:54 +0100

    PWI-26 Added missing files

---
+++ ws-pas/PasIntegrationService/AddressLookup/RecipientData.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace com.ums.pas.integration.AddressLookup
+{
+    /// <summary>
+    /// Use this to populate info about each recipient.
+    /// This is based on how the recipient was found (the target).
+    /// </summary>
+    public class RecipientData
+    {
+        public RecipientData()
+        {
+            AlertLink = new List<RefnoItem>();
+        }
+        public class RefnoItem
+        {
+            public long Refno { get; set; }
+            public int Item { get; set; }
+        }
+
+        public static RefnoItem newRefnoItem(long Refno, int Item)
+        {
+            return new RefnoItem()
+            {
+                Refno = Refno,
+                Item = Item,
+            };
+        }
+
+        /// <summary>
+        /// how the recipient was found
+        /// </summary>
+        public AlertTarget AlertTarget { get; set; }
+
+        /// <summary>
+        /// Link to alerts where this Recipient is contacted.
+        /// Attach to make sure that meta data is written for each recipient
+        /// </summary>
+        public List<RefnoItem> AlertLink { get; set; }
+
+        /// <summary>
+        /// Person/Organization name
+        /// </summary>
+        public String Name { get; set; }
+
+        /// <summary>
+        /// The endpoints that was found.
+        /// </summary>
+        public List<Endpoint> Endpoints { get; set; }
+
+    }
+}
---
+++ ws-pas/PasIntegrationService/TimeProfileDb/TimeProfileImpl.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using com.ums.UmsCommon;
+
+namespace com.ums.pas.integration.TimeProfileDb
+{
+    class TimeProfileImpl : ITimeProfilerCollector
+    {
+        public List<TimeProfile> TimeProfileList { get; private set; }
+
+        public TimeProfileImpl()
+        {
+            TimeProfileList = new List<TimeProfile>();
+        }
+
+        #region ITimeProfilerCollector Members
+
+        /// <summary>
+        /// This implementation adds to list, but also inserts into database
+        /// </summary>
+        /// <param name="Profile"></param>
+        void ITimeProfilerCollector.AddProfile(TimeProfile Profile)
+        {
+            TimeProfileList.Add(Profile);
+        }
+
+        List<TimeProfile> ITimeProfilerCollector.GetProfileList()
+        {
+            return TimeProfileList;
+        }
+
+
+
+        public void OnTimerDispose(TimeProfile Profile)
+        {
+            String Sql = String.Format("INSERT INTO MDVTIMEPROFILER(l_projectpk, sz_taskname, l_millisec, l_started_utc) VALUES({0},'{1}',{2},{3})",
+                                            Profile.Id,
+                                            Profile.TimerName,
+                                            Profile.ElapsedMsec,
+                                            Profile.Started);
+            UmsDbLib.UmsDb db = new UmsDbLib.UmsDb();
+            db.ExecNonQuery(Sql);
+            db.close();
+        }
+
+        #endregion
+    }
+}
