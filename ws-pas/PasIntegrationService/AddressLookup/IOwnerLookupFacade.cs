8ddbc9ad280dbc19509ac2cc9acb06e911fe7dc4
Morten H. Helvig <mh@ums.no>
2013-03-19 09:44:12 +0100

    PWI-85 Added functionality for multiple searches for owner
    Will first search folkereg, then revert to phonebook.

--- ws-pas/PasIntegrationService/AddressLookup/IOwnerLookupFacade.cs
+++ ws-pas/PasIntegrationService/AddressLookup/IOwnerLookupFacade.cs
@@ -7,6 +7,19 @@ namespace com.ums.pas.integration.AddressLookup
 {
     public interface IOwnerLookupFacade
     {
-        IEnumerable<RecipientData> GetMatchingOwnerAddresses(String connectionString, List<OwnerAddress> ownerAddresses);
+        /// <summary>
+        /// Get a list of owners
+        /// </summary>
+        /// <param name="ConnectionString"></param>
+        /// <param name="ownerAddresses"></param>
+        /// <returns></returns>
+        IEnumerable<RecipientData> GetMatchingOwnerAddresses(String ConnectionString, List<OwnerAddress> ownerAddresses);
+
+
+        /// <summary>
+        /// After GetMatchingoOwnerAddresses is executed, retrieve a list where there were no match.
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<OwnerAddress> GetNoMatchList();
     }
 }
