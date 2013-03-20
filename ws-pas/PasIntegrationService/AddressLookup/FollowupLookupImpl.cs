8ccb5a20bfb6a261eb3ffa64b8089ed60a196dfe
Morten H. Helvig <mh@ums.no>
2013-03-20 12:05:06 +0100

    PWI-86 Followup alert framework added

---
+++ ws-pas/PasIntegrationService/AddressLookup/FollowupLookupImpl.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using log4net;
+using System.Data.Odbc;
+
+namespace com.ums.pas.integration.AddressLookup
+{
+    class FollowupLookupImpl : IFollowupLookupFacade
+    {
+        private static ILog log = LogManager.GetLogger(typeof(FollowupLookupImpl));
+
+        #region IFollowupLookupFacade Members
+
+        /// <summary>
+        /// Connect to backbone and retrieve recipientdata from previous alerts.
+        /// </summary>
+        /// <param name="ConnectionString"></param>
+        /// <param name="FollowupAlerts"></param>
+        /// <returns></returns>
+        public IEnumerable<RecipientData> GetRecipientsFromEarlierAlerts(String ConnectionString, List<FollowupAlertObject> FollowupAlerts)
+        {
+            if (FollowupAlerts.Count == 0)
+            {
+                log.Info("No followup alerts listed");
+                return new List<RecipientData>();
+            }
+
+            List<RecipientData> recipients = new List<RecipientData>();
+            using (OdbcConnection Connection = new OdbcConnection(ConnectionString))
+            using (OdbcCommand Command = Connection.CreateCommand())
+            {
+                Connection.Open();
+
+            }
+
+            return recipients;
+        }
+
+        #endregion
+    }
+}
---
+++ ws-pas/PasIntegrationService/AddressLookup/IFollowupLookupFacade.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace com.ums.pas.integration.AddressLookup
+{
+    interface IFollowupLookupFacade
+    {
+        /// <summary>
+        /// To being able to do a resend / followup alert, this method should be used for querying
+        /// RecipientData from previous alerts.
+        /// </summary>
+        /// <param name="ConnectionString"></param>
+        /// <param name="FollowupAlerts"></param>
+        /// <returns></returns>
+        IEnumerable<RecipientData> GetRecipientsFromEarlierAlerts(String ConnectionString, List<FollowupAlertObject> FollowupAlerts);
+    }
+}
