21ea7b17cc545420cffbd8f6658964d1b120cebc
Jone Lima <jone.lima@ums.no>
2014-02-13 10:50:42 +0100

    Commented out insane sanity test

--- pas-java-client/pas-app/src/test/java/no/ums/pas/SanityTest.java
+++ pas-java-client/pas-app/src/test/java/no/ums/pas/SanityTest.java
@@ -18,9 +18,12 @@ public class SanityTest {
     public void verifyOneTimeKeyWorks() throws MalformedURLException {
     	try
     	{
+            /* This sanity test makes no sense, should check vs. the WS build for the particular realse and see that it's OK, not check that old PAS beta is matching new PAS build...
+
 	    	URL wsdl = new URL("https://secure.ums2.no/pas/ws_pasutv/ws/PAS.asmx");
 			QName service = new QName("http://ums.no/ws/pas/", "pasws");
 	        new Pasws(wsdl, service).getPaswsSoap12().getOneTimeKey();
+	        */
     	}
     	finally
     	{
