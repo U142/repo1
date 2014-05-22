b31762ce1ceb74c478085811ec70c2aa3c596b1b
Jone Lima <jone.lima@ums.no>
2014-05-22 12:56:09 +0200

    Added check for availability of TTS file and configurable retry duration

--- ws-pas/PasIntegrationService/TtsFacadeImpl.cs
+++ ws-pas/PasIntegrationService/TtsFacadeImpl.cs
@@ -22,11 +22,19 @@ namespace com.ums.pas.integration
             convertReq.n_langpk = LangPk;
             String tempPath = System.Configuration.ConfigurationManager.AppSettings["TempPath"];
             String ttsPath = System.Configuration.ConfigurationManager.AppSettings["TtsPath"];
+            int TtsRetries; int.TryParse(System.Configuration.ConfigurationManager.AppSettings["TtsRetries"], out TtsRetries); // get number of tts retries (or 0 for none)
 
             UCONVERT_TTS_RESPONSE response = new UAudio().ConvertTTS(tempPath, ttsPath, convertReq);
-            if (response.n_responsecode == 0)
+            if (response.n_responsecode == 0) // TODO: implement retries if this fails?
             {
-                byte[] rawBytes = File.ReadAllBytes(System.Configuration.ConfigurationManager.AppSettings["TtsPath"] + @"\" + response.sz_server_filename.Replace(".wav", ".raw"));
+                string ttsFile = System.Configuration.ConfigurationManager.AppSettings["TtsPath"] + @"\" + response.sz_server_filename.Replace(".wav", ".raw");
+                int tries = 0;
+
+                // check if file exists and is available for reading, try 10 * retries+1 with 100ms delay
+                while (IsFileLocked(new FileInfo(ttsFile)) && tries++ > ((TtsRetries + 1) * 10))
+                    System.Threading.Thread.Sleep(100);
+
+                byte[] rawBytes = File.ReadAllBytes(ttsFile);
                 return new AudioContent()
                 {
                     RawVersion = rawBytes,
@@ -49,7 +57,7 @@ namespace com.ums.pas.integration
             String tempPath = System.Configuration.ConfigurationManager.AppSettings["TempPath"];
             String ttsPath = System.Configuration.ConfigurationManager.AppSettings["TtsPath"];
             UCONVERT_TTS_RESPONSE response = new UAudio().ConvertTTS(tempPath, ttsPath, convertReq);
-            if (response.n_responsecode == 0)
+            if (response.n_responsecode == 0) // TODO: implement retries if this fails?
             {
                 return response.wav;
             }
@@ -57,5 +65,31 @@ namespace com.ums.pas.integration
         }
 
         #endregion
+
+        protected virtual bool IsFileLocked(FileInfo file)
+        {
+            FileStream stream = null;
+
+            try
+            {
+                stream = file.Open(FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+            }
+            catch (IOException)
+            {
+                //the file is unavailable because it is:
+                //still being written to
+                //or being processed by another thread
+                //or does not exist (has already been processed)
+                return true;
+            }
+            finally
+            {
+                if (stream != null)
+                    stream.Close();
+            }
+
+            //file is not locked
+            return false;
+        }
     }
 }
