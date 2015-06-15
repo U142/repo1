f0e21c3a53fbb61371160f03a097b3a6dbc8aa95
abhinav59 <adityaabhi.9888@gmail.com>
2015-06-15 18:03:18 +0530

    Ankit commit changed- Abhinav3

--- gitdemo1/src/gitdemo1/Ankit.java
+++ gitdemo1/src/gitdemo1/Ankit.java
@@ -2,7 +2,7 @@ package gitdemo1;
 
 public class Ankit {
 	public static int ankit;
-	public static int ankit2;
+	public static int ankit_Abhinav2;
 	public static int ankit3_Abhinav3;
 	public static int ankit_Abhinav4;
 	public static void main(String[] args) {
