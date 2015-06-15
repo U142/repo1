dfa5d6b76334819669d56d16f343c40e26f3135b
abhinav59 <adityaabhi.9888@gmail.com>
2015-06-15 14:17:03 +0530

    Ankit file changed-Abhinav

--- gitdemo1/src/gitdemo1/Ankit.java
+++ gitdemo1/src/gitdemo1/Ankit.java
@@ -4,7 +4,7 @@ public class Ankit {
 	public static int ankit;
 	public static int ankit2;
 	public static int ankit3;
-	public static int ankit4;
+	public static int ankit_Abhinav4;
 	public static void main(String[] args) {
 	
 	}
